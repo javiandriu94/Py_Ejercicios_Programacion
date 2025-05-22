c9f57c42cbcd10328da7943b90ab840b02925b75
Javier Vasquez C. <javiandriu21@gmail.com>
2025-05-22 10:57:09 -0500

    vowel and consonsant counter functio updated

--- contarVocalesConsonantes.py
+++ contarVocalesConsonantes.py
@@ -1,12 +1,12 @@
 def vowelConsonantCounter(text):
+    text2 = [char.lower()  for char in text if char.isalpha()]
     vowel=0
     consonant=0
-    for i in text:
-        if text.isalpha():
-            if i in 'aeiou':
-                vowel +=1
-            else:
-                consonant += 1
+    for i in text2:
+        if i in 'aeiou':
+            vowel +=1
+        else:
+            consonant += 1   
     return (vowel, consonant)
 
 texto = "HolaMundo! EstoEsCamelCase123 :)"
